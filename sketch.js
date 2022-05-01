ab54a985bcf675e1dd47f69bbf0588c963be1737
oscaraccorsi <91617877+oscaraccorsi@users.noreply.github.com>
2022-05-01 16:40:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
+let baseURLBackground = 'https://oscaraccorsi.github.io/backgrounds/';
 let baseURLImage = 'https://oscaraccorsi.github.io/pictures/';
-let img, logo;
+let img, logo, back;
 palette = [];
+
+let sfondoW;
+let sfondoCol, squareColor;
 
 let boxes = [];
 let co;
@@ -10,15 +14,18 @@ let fibo = [55, 89, 144, 233]
 let w;
 let h = 1;
 
+let widthRsize = 20;
+let heightRsize = 50;
+
 let limitW, limitH;
 
 let drone;
 let lowFilter; 
 
 function preload() {
-  
   drone = new Tone.Player('assets/scanner.mp3').toDestination();
-  img = loadImage(baseURLImage + 'riley57.jpeg');
+  back = loadImage(baseURLBackground + '01.png');
+  img = loadImage(baseURLImage + 'riley2.jpeg');
   logo = loadImage(baseURLImage + 'good one white.png');
 }
 
@@ -29,6 +36,7 @@ function windowResized() {
 //-----------------------------------------------SETUP
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  sfondoW = 0-width;
   xLogo = windowWidth-40;
   img.resize(100, 200);
   img.loadPixels();
@@ -36,7 +44,7 @@ function setup() {
   drone.loop = true;
   drone.autostart = true;
   
-//----------------------------------palette  
+//------------------------------------------------palette  
   for (let i=0; i < img.pixels.length; i += 4) {
     let r = img.pixels[i]; 
     let g = img.pixels[i+1]; 
@@ -44,12 +52,13 @@ function setup() {
     let c = color(r, g, b, 150);
     palette.push(c);    
   }
+  sfondoCol = random(palette);
   w = random(fibo);
   limitH = random(fibo);
   co = random(0.1, 0.01);
   for(let i = 0; i < random(numb) ; i++) {
     
-    //fill(random(palette));
+   
     noStroke();
     
     boxes[i] = {
@@ -64,28 +73,38 @@ function setup() {
 
 //------------------------------------------DRAW
 function draw() {
-  background(15, 15, 15);
+  background(20, 20, 20, 100);
+  rectMode(CORNER);
+  squareColor = sfondoCol;
+  squareColor.setAlpha(50 * sin(millis() / 3000));
+  fill(squareColor);
+  rect(sfondoW, 0, width, height);
+  sfondoW++;
   
+  if (sfondoW>width) {
+    sfondoW = 0-width;
+    sfondoCol = random(palette);
+  }
+  // image(back, 0, 0, width, height);
+  // back.resize(widthRsize, heightRsize);
+  // tint(50);
   rectMode(CENTER);
   
-  // stroke(255);
-  // for(i = 0; i < boxes.lengh; i++) {
-  //   let box = boxes[i];
+  
+  
   
   for (b of boxes) {
     fill(b.col);
     rect(b.x, b.y, w, h, 5);
-    
-    // b.x += b.speedX;
+   
     b.y += b.speedY;
 
-    // if (b.x < 100 || b.x > width-100) {
-    //   b.speedX = -b.speedX; 
-    // }
-    if (b.y < 100 || b.y > height-100) {
+    if (b.y < limitH/2 || b.y > windowHeight-limitH/2) {
+      clear();
+      clear();
       riparti();
     }
-    //w += 0.002
+    
     h += 0.002
     if (h >= limitH) {
       h = limitH;
@@ -94,19 +113,45 @@ function draw() {
       w = limitW;
     }
   }
+  
 }
 function riparti() {
+  boxes.lenght = 0;
+  clear();
   h = 1;
-  boxes = [];
-  setup();
+  notSetUp();
+}
+//-------------------------------------------notSetUp
+function notSetUp() {
+   w = random(fibo);
+  limitH = random(fibo);
+  co = random(0.1, 0.01);
+  for(let i = 0; i < random(numb) ; i++) {
+    
+    
+    noStroke();
+    
+    boxes[i] = {
+      x: random(120,width-120),
+      y: height/2,
+      speedX: random(-co, co),
+      speedY: random(-co, co),
+      col: random(palette),
+    };
+  }
 }
 
 function mousePressed() {
   imageMode(CENTER);
+  // image(back, width/2, height/2);
   logo.resize(40, 0);
   image(logo, xLogo, windowHeight-25);
-  tint(200); 
+  tint(200);
+  
+  imageMode(CORNER);
+  // background(20, 20, 20, 100);
   save();
-  clear();
-  background(15, 15, 15);
+  
+  // clear();
+  
 }
